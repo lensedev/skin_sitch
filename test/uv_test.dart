0a4fa662f2d510c9372931607a9c726024f2885e
lensedev <mattdhoy@gmail.com>
2024-05-18 12:18:23 -0500

    dart fix

--- test/uv_test.dart
+++ test/uv_test.dart
@@ -5,6 +5,11 @@ void main() async {
   test('uv.dart.fetchUv() simple test', () {
     double lat = 44.9169;
     double lng = -93.3179;
-    expect(uv.fetchUv(lat, lng), completes);
+    expect(
+        uv.fetchUv(lat, lng).then((value) {
+          print(value.now);
+          print(value.forecast);
+        }),
+        completes);
   });
 }
