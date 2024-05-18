f7433fc9795efa1067513cc2d70430c6914ece02
lensedev <mattdhoy@gmail.com>
2024-05-18 12:20:11 -0500

    removing prints

--- test/uv_test.dart
+++ test/uv_test.dart
@@ -5,11 +5,6 @@ void main() async {
   test('uv.dart.fetchUv() simple test', () {
     double lat = 44.9169;
     double lng = -93.3179;
-    expect(
-        uv.fetchUv(lat, lng).then((value) {
-          print(value.now);
-          print(value.forecast);
-        }),
-        completes);
+    expect(uv.fetchUv(lat, lng), completes);
   });
 }
